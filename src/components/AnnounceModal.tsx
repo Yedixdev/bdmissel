8c45014b7e68b29697016a64084a43364bfa478e
YedixDev <yedixonjrf@gmail.com>
2025-01-23 13:33:09 -0500

    Ajustes en el modla

--- src/components/AnnounceModal.tsx
+++ src/components/AnnounceModal.tsx
@@ -11,20 +11,20 @@ const AnnounceModal: React.FC = () => {
   if (!isOpen) return null; 
   return (
     <div className="fixed inset-0 bg-black/90 flex justify-center items-center z-50">
-      <div className="relative bg-gray-200 w-[380px] lg:w-[650px]  mt-10 lg:mt-5 p-4 rounded-lg">
+      <div className="relative bg-gray-200 w-[300px] lg:w-[650px]  mt-10 lg:mt-5 ">
         <button
           onClick={closeModal}
-          className="absolute top-2 right-2 text-xl font-bold text-gray-600 hover:text-gray-800"
+          className="absolute top-2 right-2 text-xl font-bold text-black hover:text-red-800"
         >
           X
         </button>
         <div className="flex flex-col lg:flex-row items-center justify-center gap-4">
-          <img src={Img} alt="Anuncio" className="w-60 lg:w-80" />
+          <img src={Img} alt="Anuncio" className="w-[] lg:w-[330px]" />
           <div className="flex flex-col items-center lg:items-start p-4 mt-0 lg:mt-3 gap-5">
-            <h2 className="text-2xl text-center lg:text-left text-black font-semibold font-display">
+            <h2 className="text-2xl text-center text-black font-semibold font-display">
               Tu estilo único, a tu medida
             </h2>
-            <p className="text-gray-700 hidden lg:block text-center lg:text-left">
+            <p className="text-gray-700 hidden lg:block text-center ">
               En BDMissel, personalizamos cada prenda a tu gusto, ya sea con bordados o estampados, para que tengas un diseño exclusivo. Explora nuestra amplia variedad de colores y tallas, y si no encuentras lo que buscas, ¡nosotros lo conseguimos! Si tienes una idea en mente, podemos hacerla realidad. ¡Contáctanos por WhatsApp para más detalles y haz tu pedido hoy mismo!
             </p>
             <p className="text-gray-700 text-center block lg:hidden">
